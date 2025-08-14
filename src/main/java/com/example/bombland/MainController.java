0dab2b85bcb5463350808815bd6fb81264566e83
Zul <zulkifl.alidou@gmail.com>
2025-08-14 15:51:04 -0700

    refactor: Lint MainController.java and all files that interact with it

--- src/main/java/com/example/bombland/MainController.java
+++ src/main/java/com/example/bombland/MainController.java
@@ -1,5 +1,6 @@
 package com.example.bombland;
 
+import java.io.IOException;
 import javafx.application.Platform;
 import javafx.beans.binding.Bindings;
 import javafx.event.ActionEvent;
@@ -12,140 +13,203 @@ import javafx.scene.effect.GaussianBlur;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
-import java.io.IOException;
 
+/**
+ * This class sets up the UI for the Main menu page.
+ */
 public class MainController {
-    private static MainController instance;
+  private static MainController instance;
 
-    @FXML
-    VBox mainMenuPage, mainMenuOptionsContainer, serverCommunicationErrorPopup;
+  @FXML
+  VBox mainMenuPage;
 
-    @FXML
-    StackPane mainMenuPage_stackpane;
+  @FXML
+  VBox mainMenuOptionsContainer;
 
-    @FXML
-    Text logoText_beforeO, logoText_afterO;
+  @FXML
+  VBox serverCommunicationErrorPopup;
 
-    @FXML
-    Button playBtn, instructionsBtn, highScoresBtn, exitBtn;
+  @FXML
+  StackPane mainMenuPageStackpane;
 
-    @FXML
-    Label serverCommunicationErrorPopup_title;
+  @FXML
+  Text logoTextBeforeO;
+
+  @FXML
+  Text logoTextAfterO;
+
+  @FXML
+  Button playBtn;
+
+  @FXML
+  Button instructionsBtn;
+
+  @FXML
+  Button highScoresBtn;
+
+  @FXML
+  Button exitBtn;
+
+  @FXML
+  Label serverCommunicationErrorPopupTitle;
 
 
-    private MainController() {}
+  private MainController() {}
 
-    public static MainController getInstance() {
-        if (instance == null) {
-            instance = new MainController();
-        }
-
-        return instance;
+  /**
+   * This function creates an instance of this class.
+   *
+   * @return An instance of this class.
+   */
+  public static MainController getInstance() {
+    if (instance == null) {
+      instance = new MainController();
     }
 
-    @FXML
-    public void initialize() {
-        // Prevents the width of the mainMenuOptionsContainer VBox from having the same width as its parent container (mainMenuPage)
-        mainMenuPage.setFillWidth(false);
+    return instance;
+  }
 
-        mainMenuPage_stackpane.styleProperty().bind(
-            Bindings.format("-fx-pref-width: %.2fpx; -fx-pref-height: %.2fpx;", Main.mainStage.widthProperty().multiply(0.75), Main.mainStage.heightProperty().multiply(0.6))
-        );
+  /**
+   * Creates and populates the Main menu page.
+   */
+  @FXML
+  public void initialize() {
+    // Prevents the width of the mainMenuOptionsContainer VBox
+    // from having the same width as its parent container (mainMenuPage)
+    mainMenuPage.setFillWidth(false);
 
-
-        mainMenuOptionsContainer.styleProperty().bind(
-                Bindings.format("-fx-pref-width: %.2fpx; -fx-pref-height: %.2fpx;", Main.mainStage.widthProperty().multiply(0.75), Main.mainStage.heightProperty().multiply(0.6))
-        );
-
-
-        logoText_beforeO.styleProperty().bind(
-                // Sets the font size to be 9% of the app window's width
-                Bindings.format("-fx-font-size: %.2fpx; -fx-font-weight: bold;", Main.mainStage.widthProperty().multiply(0.045))
-        );
-
-        logoText_afterO.styleProperty().bind(
-                // Sets the font size to be 9% of the app window's width
-                Bindings.format("-fx-font-size: %.2fpx; -fx-font-weight: bold;", Main.mainStage.widthProperty().multiply(0.045))
-        );
+    mainMenuPageStackpane.styleProperty().bind(
+        Bindings.format("-fx-pref-width: %.2fpx; -fx-pref-height: %.2fpx;",
+            Main.mainStage.widthProperty().multiply(0.75),
+            Main.mainStage.heightProperty().multiply(0.6))
+    );
 
 
-        playBtn.styleProperty().bind(
-                Bindings.format("-fx-pref-width: %.2fpx; -fx-font-size: %.2fpx; -fx-background-radius: %.2fpx;", Main.mainStage.widthProperty().multiply(0.4), Main.mainStage.widthProperty().multiply(0.03), Main.mainStage.widthProperty().multiply(0.1))
-        );
+    mainMenuOptionsContainer.styleProperty().bind(
+        Bindings.format("-fx-pref-width: %.2fpx; -fx-pref-height: %.2fpx;",
+            Main.mainStage.widthProperty().multiply(0.75),
+            Main.mainStage.heightProperty().multiply(0.6))
+    );
 
-        instructionsBtn.styleProperty().bind(
-                Bindings.format("-fx-pref-width: %.2fpx; -fx-font-size: %.2fpx; -fx-background-radius: %.2fpx;", Main.mainStage.widthProperty().multiply(0.4), Main.mainStage.widthProperty().multiply(0.03), Main.mainStage.widthProperty().multiply(0.1))
-        );
 
-        highScoresBtn.styleProperty().bind(
-                Bindings.format("-fx-pref-width: %.2fpx; -fx-font-size: %.2fpx; -fx-background-radius: %.2fpx;", Main.mainStage.widthProperty().multiply(0.4), Main.mainStage.widthProperty().multiply(0.03), Main.mainStage.widthProperty().multiply(0.1))
-        );
+    logoTextBeforeO.styleProperty().bind(
+        // Sets the font size to be 9% of the app window's width
+        Bindings.format("-fx-font-size: %.2fpx; -fx-font-weight: bold;",
+            Main.mainStage.widthProperty().multiply(0.045))
+    );
 
-        exitBtn.styleProperty().bind(
-                Bindings.format("-fx-pref-width: %.2fpx; -fx-font-size: %.2fpx; -fx-background-radius: %.2fpx;", Main.mainStage.widthProperty().multiply(0.4), Main.mainStage.widthProperty().multiply(0.03), Main.mainStage.widthProperty().multiply(0.1))
-        );
-    }
+    logoTextAfterO.styleProperty().bind(
+        // Sets the font size to be 9% of the app window's width
+        Bindings.format("-fx-font-size: %.2fpx; -fx-font-weight: bold;",
+            Main.mainStage.widthProperty().multiply(0.045))
+    );
 
-    @FXML
-    private void openDifficultySelectionPage(ActionEvent event) throws IOException {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/bombland/FXML/difficulty-selection-view.fxml"));
 
-        DifficultySelectionController difficultyController = DifficultySelectionController.getInstance();
-        loader.setController(difficultyController);
+    playBtn.styleProperty().bind(
+        Bindings.format("-fx-pref-width: %.2fpx; -fx-font-size: %.2fpx; "
+            + "-fx-background-radius: %.2fpx;",
+            Main.mainStage.widthProperty().multiply(0.4),
+            Main.mainStage.widthProperty().multiply(0.03),
+            Main.mainStage.widthProperty().multiply(0.1))
+    );
 
-        Scene scene = new Scene(loader.load(), 1024, 768);
-        Main.mainStage.setScene(scene);
-        Main.mainStage.show();
-    }
+    instructionsBtn.styleProperty().bind(
+        Bindings.format("-fx-pref-width: %.2fpx; -fx-font-size: %.2fpx; "
+            + "-fx-background-radius: %.2fpx;",
+            Main.mainStage.widthProperty().multiply(0.4),
+            Main.mainStage.widthProperty().multiply(0.03),
+            Main.mainStage.widthProperty().multiply(0.1))
+    );
 
-    @FXML
-    private void openInstructionsPage(ActionEvent event) throws IOException {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/bombland/FXML/instructions-view.fxml"));
+    highScoresBtn.styleProperty().bind(
+        Bindings.format("-fx-pref-width: %.2fpx; -fx-font-size: %.2fpx; "
+            + "-fx-background-radius: %.2fpx;",
+            Main.mainStage.widthProperty().multiply(0.4),
+            Main.mainStage.widthProperty().multiply(0.03),
+            Main.mainStage.widthProperty().multiply(0.1))
+    );
 
-        InstructionsController instructionsController = InstructionsController.getInstance();
-        loader.setController(instructionsController);
+    exitBtn.styleProperty().bind(
+        Bindings.format("-fx-pref-width: %.2fpx; -fx-font-size: %.2fpx; "
+            + "-fx-background-radius: %.2fpx;",
+            Main.mainStage.widthProperty().multiply(0.4),
+            Main.mainStage.widthProperty().multiply(0.03),
+            Main.mainStage.widthProperty().multiply(0.1))
+    );
+  }
 
-        Scene scene = new Scene(loader.load(), 1024, 768);
-        Main.mainStage.setScene(scene);
-        Main.mainStage.show();
-    }
+  @FXML
+  private void openDifficultySelectionPage(ActionEvent event) throws IOException {
+    FXMLLoader loader = new FXMLLoader(
+        getClass().getResource("/com/example/bombland/FXML/difficulty-selection-view.fxml")
+    );
 
-    @FXML
-    private void openHighScoresPage(ActionEvent event) throws IOException {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/bombland/FXML/high-scores-view.fxml"));
+    DifficultySelectionController difficultyController = DifficultySelectionController.getInstance();
+    loader.setController(difficultyController);
 
-        HighScoresController highScoresController = HighScoresController.getInstance();
-        loader.setController(highScoresController);
+    Scene scene = new Scene(loader.load(), 1024, 768);
+    Main.mainStage.setScene(scene);
+    Main.mainStage.show();
+  }
 
-        Scene scene = new Scene(loader.load(), 1024, 768);
-        Main.mainStage.setScene(scene);
-        Main.mainStage.show();
-    }
+  @FXML
+  private void openInstructionsPage(ActionEvent event) throws IOException {
+    FXMLLoader loader = new FXMLLoader(
+        getClass().getResource("/com/example/bombland/FXML/instructions-view.fxml")
+    );
 
-    @FXML
-    private void closeApp() {
-        Platform.exit();
-    }
+    InstructionsController instructionsController = InstructionsController.getInstance();
+    loader.setController(instructionsController);
 
-    public void displayServerErrorPopup() {
-        mainMenuOptionsContainer.setEffect(new GaussianBlur());
-        mainMenuOptionsContainer.setMouseTransparent(true);
+    Scene scene = new Scene(loader.load(), 1024, 768);
+    Main.mainStage.setScene(scene);
+    Main.mainStage.show();
+  }
 
-        serverCommunicationErrorPopup.setManaged(true);
-        serverCommunicationErrorPopup.setVisible(true);
+  @FXML
+  private void openHighScoresPage(ActionEvent event) throws IOException {
+    FXMLLoader loader = new FXMLLoader(
+        getClass().getResource("/com/example/bombland/FXML/high-scores-view.fxml")
+    );
 
-        serverCommunicationErrorPopup.setMaxWidth(500);
-        serverCommunicationErrorPopup.setMaxHeight(400);
-        serverCommunicationErrorPopup.setStyle("-fx-background-radius: 10px;");
+    HighScoresController highScoresController = HighScoresController.getInstance();
+    loader.setController(highScoresController);
 
-        serverCommunicationErrorPopup_title.setStyle("-fx-font-size: 25px;");
-    }
+    Scene scene = new Scene(loader.load(), 1024, 768);
+    Main.mainStage.setScene(scene);
+    Main.mainStage.show();
+  }
 
-    public void closeServerErrorPopup() {
-        serverCommunicationErrorPopup.setManaged(false);
-        serverCommunicationErrorPopup.setVisible(false);
+  @FXML
+  private void closeApp() {
+    Platform.exit();
+  }
 
-        mainMenuOptionsContainer.setEffect(null);
-        mainMenuOptionsContainer.setMouseTransparent(false);
-    }
+  /**
+   * This function displays a server error popup.
+   */
+  public void displayServerErrorPopup() {
+    mainMenuOptionsContainer.setEffect(new GaussianBlur());
+    mainMenuOptionsContainer.setMouseTransparent(true);
+
+    serverCommunicationErrorPopup.setManaged(true);
+    serverCommunicationErrorPopup.setVisible(true);
+
+    serverCommunicationErrorPopup.setMaxWidth(500);
+    serverCommunicationErrorPopup.setMaxHeight(400);
+    serverCommunicationErrorPopup.setStyle("-fx-background-radius: 10px;");
+
+    serverCommunicationErrorPopupTitle.setStyle("-fx-font-size: 25px;");
+  }
+
+  /**
+   * This function closes a server error popup.
+   */
+  public void closeServerErrorPopup() {
+    serverCommunicationErrorPopup.setManaged(false);
+    serverCommunicationErrorPopup.setVisible(false);
+
+    mainMenuOptionsContainer.setEffect(null);
+    mainMenuOptionsContainer.setMouseTransparent(false);
+  }
 }
