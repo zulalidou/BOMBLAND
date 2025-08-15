cda510c1777470701c3d73a21256374632977aa1
Zul <zulkifl.alidou@gmail.com>
2025-08-15 16:38:00 -0700

    refactor: Lint Tile.java and all files that interact with it

--- src/main/java/com/example/bombland/Tile.java
+++ src/main/java/com/example/bombland/Tile.java
@@ -1,29 +1,36 @@
 package com.example.bombland;
 
-import javafx.scene.control.*;
+import javafx.scene.control.Button;
 
+/**
+ * This class creates and manages the states of each tile.
+ */
 public class Tile {
-    boolean isCovered, isFlagged;
-    TileValue value;
-    String backgroundFile;
-    int row, col, surroundingBombs;
-    Button tileBtn;
+  boolean isCovered;
+  boolean isFlagged;
+  TileValue value;
+  String backgroundFile;
+  int row;
+  int col;
+  int surroundingBombs;
+  Button tileBtn;
 
-    enum TileValue {
-        UNKNOWN,
-        EMPTY,
-        NUMBER,
-        BOMB,
-        DISABLED
-    }
+  enum TileValue {
+    UNKNOWN,
+    EMPTY,
+    NUMBER,
+    BOMB,
+    DISABLED
+  }
 
-    Tile(Button tb) {
-        isCovered = true;
-        isFlagged = false;
-        value = TileValue.UNKNOWN;
-        backgroundFile = "";
-        row = col = -1;
-        surroundingBombs = 0;
-        tileBtn = tb;
-    }
+  Tile(Button tb) {
+    isCovered = true;
+    isFlagged = false;
+    value = TileValue.UNKNOWN;
+    backgroundFile = "";
+    row = -1;
+    col = -1;
+    surroundingBombs = 0;
+    tileBtn = tb;
+  }
 }
