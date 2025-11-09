5caf799c64e7ce032e1e073c44f4eb8dfe410a83
Zul-Kifl Alidou <zulkifl.alidou@gmail.com>
2025-11-08 17:54:31 -0800

    feat: Make Instructions page responsive

--- src/main/java/com/example/bombland/InstructionsController.java
+++ src/main/java/com/example/bombland/InstructionsController.java
@@ -2,7 +2,6 @@ package com.example.bombland;
 
 import java.io.IOException;
 import javafx.beans.binding.Bindings;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -22,22 +21,28 @@ public class InstructionsController {
   private static InstructionsController instance;
 
   @FXML
-  VBox instructionsPage;
-
-  @FXML
-  Button backBtn;
-
-  @FXML
-  Label instructionsPageTitle;
+  HBox instructionsContainerTop;
 
   @FXML
   HBox instructionsContainerLeftChild;
 
   @FXML
+  Button backBtn;
+
+  @FXML
+  ImageView backBtnImgView;
+
+  @FXML
   HBox instructionsContainerMiddleChild;
 
   @FXML
+  Label instructionsPageTitle;
+
+  @FXML
   HBox instructionsContainerRightChild;
+
+  @FXML
+  HBox instructionsContainerBottom;
 
   @FXML
   ScrollPane instructionsScrollPane;
@@ -49,61 +54,61 @@ public class InstructionsController {
   VBox imgContainer1;
 
   @FXML
-  VBox imgContainer2;
-
-  @FXML
-  VBox imgContainer3;
-
-  @FXML
-  VBox imgContainer4;
-
-  @FXML
-  VBox imgContainer5;
-
-  @FXML
-  VBox imgContainer6;
-
-  @FXML
-  VBox imgContainer7;
-
-  @FXML
-  VBox imgContainer8;
-
-  @FXML
   ImageView imgView1;
-
-  @FXML
-  ImageView imgView2;
-
-  @FXML
-  ImageView imgView3;
-
-  @FXML
-  ImageView imgView4;
-
-  @FXML
-  ImageView imgView5;
-
-  @FXML
-  ImageView imgView6;
-
-  @FXML
-  ImageView imgView7;
-
-  @FXML
-  ImageView imgView8;
 
   @FXML
   Region space1;
 
   @FXML
+  VBox imgContainer2;
+
+  @FXML
+  ImageView imgView2;
+
+  @FXML
+  VBox imgContainer3;
+
+  @FXML
+  ImageView imgView3;
+
+  @FXML
   Region space2;
+
+  @FXML
+  VBox imgContainer4;
+
+  @FXML
+  ImageView imgView4;
+
+  @FXML
+  VBox imgContainer5;
+
+  @FXML
+  ImageView imgView5;
 
   @FXML
   Region space3;
 
   @FXML
+  VBox imgContainer6;
+
+  @FXML
+  ImageView imgView6;
+
+  @FXML
+  VBox imgContainer7;
+
+  @FXML
+  ImageView imgView7;
+
+  @FXML
   Region space4;
+
+  @FXML
+  VBox imgContainer8;
+
+  @FXML
+  ImageView imgView8;
 
 
   private InstructionsController() {}
@@ -126,104 +131,74 @@ public class InstructionsController {
    */
   @FXML
   public void initialize() {
-    instructionsPage.styleProperty().bind(
-        Bindings.format("-fx-padding: %.2fpx;", Main.mainStage.widthProperty().multiply(0.02))
+    instructionsContainerTop.setPrefHeight(Main.mainStage.getHeight() * 0.1);
+
+    // In some screen resolutions the page header/title glitches, so the line below prevents that
+    instructionsContainerTop.setMinHeight(Main.mainStage.getHeight() * 0.1);
+
+    instructionsContainerLeftChild.styleProperty().bind(
+        Bindings.format("-fx-pref-width: %.2fpx;", instructionsContainerTop.widthProperty().multiply(0.33))
     );
 
-
     backBtn.styleProperty().bind(
-        Bindings.format("-fx-background-radius: %.2fpx;",
-            Main.mainStage.widthProperty().multiply(0.05))
+        Bindings.format("-fx-background-radius: %.2fpx;", instructionsContainerLeftChild.widthProperty().multiply(0.1))
+    );
+
+    backBtnImgView.fitWidthProperty().bind(instructionsContainerLeftChild.widthProperty().multiply(0.075));
+    backBtnImgView.fitHeightProperty().bind(instructionsContainerLeftChild.heightProperty().multiply(0.4));
+
+    instructionsContainerMiddleChild.styleProperty().bind(
+        Bindings.format("-fx-pref-width: %.2fpx; -fx-pref-height: %.2fpx",
+            instructionsContainerTop.widthProperty().multiply(0.34),
+            instructionsContainerTop.heightProperty().multiply(1))
     );
 
     instructionsPageTitle.styleProperty().bind(
-        Bindings.format("-fx-font-size: %.2fpx;", Main.mainStage.widthProperty().multiply(0.04))
-    );
-
-    instructionsContainerLeftChild.styleProperty().bind(
-        Bindings.format("-fx-pref-width: %.2fpx;", Main.mainStage.widthProperty().multiply(0.33))
-    );
-
-    instructionsContainerMiddleChild.styleProperty().bind(
-        Bindings.format("-fx-pref-width: %.2fpx", Main.mainStage.widthProperty().multiply(0.34))
+        Bindings.format("-fx-font-size: %.2fpx;", instructionsContainerMiddleChild.widthProperty().multiply(0.1))
     );
 
     instructionsContainerRightChild.styleProperty().bind(
-        Bindings.format("-fx-pref-width: %.2fpx;", Main.mainStage.widthProperty().multiply(0.33))
+        Bindings.format("-fx-pref-width: %.2fpx;", instructionsContainerTop.widthProperty().multiply(0.33))
     );
 
+    VBox.setVgrow(instructionsContainerBottom, Priority.ALWAYS);
 
     instructionsScrollPane.styleProperty().bind(
-        Bindings.format("-fx-padding: %.2fpx;", Main.mainStage.widthProperty().multiply(0.02))
+        Bindings.format("-fx-padding: %.2fpx;", instructionsContainerBottom.widthProperty().multiply(0.02))
     );
 
-    // Makes sure the child of the scrollpane has the same width as the scrollpane
+    // Makes sure the child of the scroll pane has the same width as the scroll pane
     instructionsScrollPane.setFitToWidth(true);
-    VBox.setVgrow(instructionsScrollPane, Priority.ALWAYS);
-
 
     section1TitleLbl.styleProperty().bind(
-            Bindings.format("-fx-font-size: %.2fpx;", Main.mainStage.widthProperty().multiply(0.03))
+        Bindings.format("-fx-font-size: %.2fpx;", instructionsScrollPane.widthProperty().multiply(0.025))
     );
 
+    imgContainer1.setPrefWidth(Main.mainStage.getWidth() * (0.45));
+    imgContainer2.setPrefWidth(Main.mainStage.getWidth() * (0.45));
+    imgContainer3.setPrefWidth(Main.mainStage.getWidth() * (0.45));
+    imgContainer4.setPrefWidth(Main.mainStage.getWidth() * (0.45));
+    imgContainer5.setPrefWidth(Main.mainStage.getWidth() * (0.45));
+    imgContainer6.setPrefWidth(Main.mainStage.getWidth() * (0.45));
+    imgContainer7.setPrefWidth(Main.mainStage.getWidth() * (0.45));
+    imgContainer8.setPrefWidth(Main.mainStage.getWidth() * (0.45));
 
-    imgContainer1.styleProperty().bind(
-            Bindings.format("-fx-padding: %.2fpx; -fx-pref-width: %.2fpx;",
-                Main.mainStage.widthProperty().multiply(0.01),
-                Main.mainStage.widthProperty().multiply(0.4))
-    );
-    imgContainer2.styleProperty().bind(
-            Bindings.format("-fx-padding: %.2fpx; -fx-pref-width: %.2fpx;",
-                Main.mainStage.widthProperty().multiply(0.01),
-                Main.mainStage.widthProperty().multiply(0.4))
-    );
-    imgContainer3.styleProperty().bind(
-            Bindings.format("-fx-padding: %.2fpx; -fx-pref-width: %.2fpx;",
-                Main.mainStage.widthProperty().multiply(0.01),
-                Main.mainStage.widthProperty().multiply(0.4))
-    );
-    imgContainer4.styleProperty().bind(
-            Bindings.format("-fx-padding: %.2fpx; -fx-pref-width: %.2fpx;",
-                Main.mainStage.widthProperty().multiply(0.01),
-                Main.mainStage.widthProperty().multiply(0.4))
-    );
-    imgContainer5.styleProperty().bind(
-            Bindings.format("-fx-padding: %.2fpx; -fx-pref-width: %.2fpx;",
-                Main.mainStage.widthProperty().multiply(0.01),
-                Main.mainStage.widthProperty().multiply(0.4))
-    );
-    imgContainer6.styleProperty().bind(
-            Bindings.format("-fx-padding: %.2fpx; -fx-pref-width: %.2fpx;",
-                Main.mainStage.widthProperty().multiply(0.01),
-                Main.mainStage.widthProperty().multiply(0.4))
-    );
-    imgContainer7.styleProperty().bind(
-            Bindings.format("-fx-padding: %.2fpx; -fx-pref-width: %.2fpx;",
-                Main.mainStage.widthProperty().multiply(0.01),
-                Main.mainStage.widthProperty().multiply(0.4))
-    );
-    imgContainer8.styleProperty().bind(
-            Bindings.format("-fx-padding: %.2fpx; -fx-pref-width: %.2fpx;",
-                Main.mainStage.widthProperty().multiply(0.01),
-                Main.mainStage.widthProperty().multiply(0.4))
-    );
-
-    imgView1.setFitWidth(Main.mainStage.getWidth() * 0.35);
-    imgView1.setFitHeight(Main.mainStage.getHeight() * 0.25);
-    imgView2.setFitWidth(Main.mainStage.getWidth() * 0.35);
-    imgView2.setFitHeight(Main.mainStage.getHeight() * 0.25);
-    imgView3.setFitWidth(Main.mainStage.getWidth() * 0.35);
-    imgView3.setFitHeight(Main.mainStage.getHeight() * 0.25);
-    imgView4.setFitWidth(Main.mainStage.getWidth() * 0.35);
-    imgView4.setFitHeight(Main.mainStage.getHeight() * 0.25);
-    imgView5.setFitWidth(Main.mainStage.getWidth() * 0.35);
-    imgView5.setFitHeight(Main.mainStage.getHeight() * 0.25);
-    imgView6.setFitWidth(Main.mainStage.getWidth() * 0.35);
-    imgView6.setFitHeight(Main.mainStage.getHeight() * 0.25);
-    imgView7.setFitWidth(Main.mainStage.getWidth() * 0.35);
-    imgView7.setFitHeight(Main.mainStage.getHeight() * 0.25);
-    imgView8.setFitWidth(Main.mainStage.getWidth() * 0.35);
-    imgView8.setFitHeight(Main.mainStage.getHeight() * 0.25);
+    imgView1.fitWidthProperty().bind(imgContainer1.widthProperty().multiply(0.5));
+    imgView1.setPreserveRatio(true);
+    imgView2.fitWidthProperty().bind(imgContainer2.widthProperty().multiply(0.5));
+    imgView2.setPreserveRatio(true);
+    imgView3.fitWidthProperty().bind(imgContainer3.widthProperty().multiply(0.5));
+    imgView3.setPreserveRatio(true);
+    imgView4.fitWidthProperty().bind(imgContainer4.widthProperty().multiply(0.5));
+    imgView4.setPreserveRatio(true);
+    imgView5.fitWidthProperty().bind(imgContainer5.widthProperty().multiply(0.5));
+    imgView5.setPreserveRatio(true);
+    imgView6.fitWidthProperty().bind(imgContainer6.widthProperty().multiply(0.5));
+    imgView6.setPreserveRatio(true);
+    imgView7.fitWidthProperty().bind(imgContainer7.widthProperty().multiply(0.5));
+    imgView7.setPreserveRatio(true);
+    imgView8.fitWidthProperty().bind(imgContainer8.widthProperty().multiply(0.5));
+    imgView8.setPreserveRatio(true);
 
     HBox.setHgrow(space1, Priority.ALWAYS); // Makes the space element expand
     HBox.setHgrow(space2, Priority.ALWAYS);
@@ -231,8 +206,11 @@ public class InstructionsController {
     HBox.setHgrow(space4, Priority.ALWAYS);
   }
 
+  /**
+   * Redirects the user to the Main Menu page.
+   */
   @FXML
-  private void goToMainMenu(ActionEvent event) {
+  private void goToMainMenu() {
     FXMLLoader loader = new FXMLLoader(
         getClass().getResource("/com/example/bombland/FXML/main-view.fxml")
     );
